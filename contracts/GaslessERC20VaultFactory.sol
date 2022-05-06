7b999575881d2336acf243f22366f592141b5887
Steven Enamakel <enamakel@mahadao.com>
2022-05-06 13:51:40 +0100

    uncommented the typehash

--- contracts/GaslessERC20VaultFactory.sol
+++ contracts/GaslessERC20VaultFactory.sol
@@ -3,48 +3,63 @@ pragma solidity ^0.8.0;
 
 import {GaslessERC20Vault} from "./GaslessERC20Vault.sol";
 
-struct EIP712Domain {
-    string name;
-    string version;
-    uint256 chainId;
-    address verifyingContract;
-}
-
-struct MetaTransaction {
-    uint256 nonce;
-    address from;
-}
-
 // GaslessERC20VaultFactory
 // This factory deploys new proxy instances through build()
 // Deployed proxy addresses are logged
 contract GaslessERC20VaultFactory {
     uint256 constant chainID = 56;
+
+    struct EIP712Domain {
+        string name;
+        string version;
+        uint256 chainId;
+        address verifyingContract;
+    }
+
+    struct MetaTransactionBuild {
+        uint256 nonce;
+        address from;
+    }
+
+    struct MetaTransactionTransfer {
+        uint256 nonce;
+        address from;
+        address to;
+        uint256 amount;
+    }
+
     mapping(address => uint256) public nonces;
     mapping(address => address) public vaults;
 
     event Created(address indexed vault, address owner);
 
-    // bytes32 public constant METATRANSACTION_TYPEHASH =
-    //     keccak256(bytes("MetaTransaction(uint256 nonce, address from)"));
+    bytes32 public constant METATRANSACTION_BUILD_TYPEHASH =
+        keccak256(bytes("MetaTransactionBuild(uint256 nonce, address from)"));
 
-    // bytes32 public constant EIP712_DOMAIN_TYPEHASH =
-    //     keccak256(
-    //         bytes(
-    //             "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
-    //         )
-    //     );
+    bytes32 public constant METATRANSACTION_TRANSFER_TYPEHASH =
+        keccak256(
+            bytes(
+                "MetaTransactionTransfer(uint256 nonce, address from, address to, uint256 amount)"
+            )
+        );
 
-    // bytes32 public DOMAIN_SEPARATOR =
-    //     keccak256(
-    //         abi.encode(
-    //             EIP712_DOMAIN_TYPEHASH,
-    //             "build",
-    //             "1",
-    //             chainID,
-    //             address(this)
-    //         )
-    //     );
+    bytes32 public constant EIP712_DOMAIN_TYPEHASH =
+        keccak256(
+            bytes(
+                "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+            )
+        );
+
+    bytes32 public DOMAIN_SEPARATOR =
+        keccak256(
+            abi.encode(
+                EIP712_DOMAIN_TYPEHASH,
+                "build",
+                "1",
+                chainID,
+                address(this)
+            )
+        );
 
     // deploys a new proxy instance
     // sets custom owner of proxy
@@ -54,24 +69,24 @@ contract GaslessERC20VaultFactory {
         bytes32 s,
         uint8 v
     ) public returns (address payable vault) {
-        // MetaTransaction memory metaTx = MetaTransaction({
-        //     nonce: nonces[_owner],
-        //     from: _owner
-        // });
+        MetaTransactionBuild memory metaTx = MetaTransactionBuild({
+            nonce: nonces[_owner],
+            from: _owner
+        });
 
-        // bytes32 digest = keccak256(
-        //     abi.encodePacked(
-        //         "\\x19\\x01",
-        //         DOMAIN_SEPARATOR,
-        //         keccak256(
-        //             abi.encode(
-        //                 METATRANSACTION_TYPEHASH,
-        //                 metaTx.nonce,
-        //                 metaTx.from
-        //             )
-        //         )
-        //     )
-        // );
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\\x19\\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(
+                    abi.encode(
+                        METATRANSACTION_BUILD_TYPEHASH,
+                        metaTx.nonce,
+                        metaTx.from
+                    )
+                )
+            )
+        );
 
         // Verify the _owner with the address recovered from the signatures
         // require(_owner == ecrecover(digest, v, r, s), "invalid-signatures");
@@ -95,24 +110,28 @@ contract GaslessERC20VaultFactory {
         bytes32 s,
         uint8 v
     ) external {
-        // MetaTransaction memory metaTx = MetaTransaction({
-        //     nonce: nonces[_owner],
-        //     from: _owner
-        // });
+        MetaTransactionTransfer memory metaTx = MetaTransactionTransfer({
+            nonce: nonces[from],
+            from: from,
+            to: to,
+            amount: amount
+        });
 
-        // bytes32 digest = keccak256(
-        //     abi.encodePacked(
-        //         "\\x19\\x01",
-        //         DOMAIN_SEPARATOR,
-        //         keccak256(
-        //             abi.encode(
-        //                 METATRANSACTION_TYPEHASH,
-        //                 metaTx.nonce,
-        //                 metaTx.from
-        //             )
-        //         )
-        //     )
-        // );
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\\x19\\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(
+                    abi.encode(
+                        METATRANSACTION_TRANSFER_TYPEHASH,
+                        metaTx.nonce,
+                        metaTx.from,
+                        metaTx.to,
+                        metaTx.amount
+                    )
+                )
+            )
+        );
 
         // Verify the _owner with the address recovered from the signatures
         // require(_owner == ecrecover(digest, v, r, s), "invalid-signatures");
