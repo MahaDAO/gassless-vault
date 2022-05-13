1242ee0b10b2e8dfa758f7bf2196753e293b2740
Elvin-Skaria <encorereloded@gmail.com>
2022-05-13 16:41:52 +0530

    Gasless Contract without user verification

--- contracts/GaslessERC20VaultFactory.sol
+++ contracts/GaslessERC20VaultFactory.sol
@@ -7,139 +7,31 @@ import {GaslessERC20Vault} from "./GaslessERC20Vault.sol";
 // This factory deploys new proxy instances through build()
 // Deployed proxy addresses are logged
 contract GaslessERC20VaultFactory {
-    uint256 constant chainID = 56;
-
-    struct EIP712Domain {
-        string name;
-        string version;
-        uint256 chainId;
-        address verifyingContract;
-    }
-
-    struct MetaTransactionBuild {
-        uint256 nonce;
-        address from;
-    }
-
-    struct MetaTransactionTransfer {
-        uint256 nonce;
-        address from;
-        address to;
-        uint256 amount;
-    }
-
     mapping(address => uint256) public nonces;
     mapping(address => address) public vaults;
 
     event Created(address indexed vault, address owner);
 
-    bytes32 public constant METATRANSACTION_BUILD_TYPEHASH =
-        keccak256(bytes("MetaTransactionBuild(uint256 nonce, address from)"));
-
-    bytes32 public constant METATRANSACTION_TRANSFER_TYPEHASH =
-        keccak256(
-            bytes(
-                "MetaTransactionTransfer(uint256 nonce, address from, address to, uint256 amount)"
-            )
-        );
-
-    bytes32 public constant EIP712_DOMAIN_TYPEHASH =
-        keccak256(
-            bytes(
-                "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
-            )
-        );
-
-    bytes32 public DOMAIN_SEPARATOR =
-        keccak256(
-            abi.encode(
-                EIP712_DOMAIN_TYPEHASH,
-                "build",
-                "1",
-                chainID,
-                address(this)
-            )
-        );
-
     // deploys a new proxy instance
     // sets custom owner of proxy
-    function build(
-        address _owner,
-        bytes32 r,
-        bytes32 s,
-        uint8 v
-    ) public returns (address payable vault) {
-        MetaTransactionBuild memory metaTx = MetaTransactionBuild({
-            nonce: nonces[_owner],
-            from: _owner
-        });
-
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\\x19\\x01",
-                DOMAIN_SEPARATOR,
-                keccak256(
-                    abi.encode(
-                        METATRANSACTION_BUILD_TYPEHASH,
-                        metaTx.nonce,
-                        metaTx.from
-                    )
-                )
-            )
-        );
-
-        // Verify the _owner with the address recovered from the signatures
-        // require(_owner == ecrecover(digest, v, r, s), "invalid-signatures");
-
+    function build(address _owner) public returns (address vault) {
         require(vaults[_owner] == address(0), "Vault already created");
 
-        // Verify the _owner is not address zero
-        require(_owner != address(0), "invalid-address-0");
-
         vault = payable(address(new GaslessERC20Vault(_owner, address(this))));
+
         emit Created(vault, _owner);
         vaults[_owner] = vault;
     }
 
     function transferToken(
-        address token,
+        address _token,
         address from,
         address to,
-        uint256 amount,
-        bytes32 r,
-        bytes32 s,
-        uint8 v
+        uint256 amount
     ) external {
-        MetaTransactionTransfer memory metaTx = MetaTransactionTransfer({
-            nonce: nonces[from],
-            from: from,
-            to: to,
-            amount: amount
-        });
-
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\\x19\\x01",
-                DOMAIN_SEPARATOR,
-                keccak256(
-                    abi.encode(
-                        METATRANSACTION_TRANSFER_TYPEHASH,
-                        metaTx.nonce,
-                        metaTx.from,
-                        metaTx.to,
-                        metaTx.amount
-                    )
-                )
-            )
-        );
-
-        // Verify the _owner with the address recovered from the signatures
-        // require(_owner == ecrecover(digest, v, r, s), "invalid-signatures");
-
-        // Verify the _owner is not address zero
         require(vaults[from] != address(0), "no vault for user");
 
         GaslessERC20Vault vault = GaslessERC20Vault(vaults[from]);
-        vault.transferToken(token, amount, to);
+        vault.transferToken(_token, amount, to);
     }
 }
